1a18a20ba2529a298a4ebf94e584291d39987197
somarcamila <camirgarzon@gmail.com>
2021-08-21 15:35:58 -0700

    slice method not working, need to debug why my checks for null/undefined aren't working

--- src/Components/CompleteProfile.js
+++ src/Components/CompleteProfile.js
@@ -60,24 +60,23 @@ export default function CompleteProfile(props){
       email: ''
     });
     const [loading, setLoading] = useState(true);
-    const userIndex = props.selected;
+    //const userIndex = props.selected;
     const USER_ID = props.response.id
 
     const getFullProfile = async () => {
       console.log("function is getting callsed");
       const fullProfile = await axios.get(`${BASE_URL}/user/${USER_ID}`, { headers: {'app-id': APP_ID} })
-      console.log("am i trippin?", fullProfile.data.dateOfBirth);
-      setUserInfo({
-        dateOfBirth: fullProfile.data.dateOfBirth,
-        phone: fullProfile.data.phone,
-        email: fullProfile.data.email
-      })
-      // setUserInfo(fullProfile.data);
-      //console.log("wahts in fullprofile", fullProfile.data);
+      console.log("am i trippin?", fullProfile.data); // prints the response no problem
+      setUserInfo(fullProfile.data);
+      console.log(userInfo) // prints null?? 
+      // setUserInfo({
+      //   dateOfBirth: fullProfile.data.dateOfBirth,
+      //   phone: fullProfile.data.phone,
+      //   email: fullProfile.data.email
+      // })
       console.log("userInfo!!!!!!", userInfo);
       setLoading(false);
       console.log("whats the status of loading", loading);
-      return fullProfile.data;
   }
 
 
@@ -91,97 +90,159 @@ export default function CompleteProfile(props){
         }
     }, [USER_ID]);
 
-    // const makeRequest = () => {
-    //     console.log("does the control even call this at all");
-    //     if (props.selected !== null){
-    //         console.log("making request");
-    //     axios.get(`${BASE_URL}/user/${userID}`, { headers: {'app-id': APP_ID} })
-    //     .then(({data}) => setUserInfo(data.data))
-    //     .catch(console.error)
-    //     .finally(() => setLoading(false));
-    //     console.log("did we get the resopnse", userInfo);
-    //     }
-    // }
-
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
       };
 
-
-    return (
-        <Card className={classes.root}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="recipe" className={classes.avatar}>
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title= {props.firstName}
-          subheader={props.title}
-        />
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/paella.jpg"
-          title="Paella dish"
-        />
+      const view = (userInfo === null || userInfo === undefined) ? null : <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {userInfo.firstName}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title= {props.firstName}
+        subheader={props.title}
+      />
+      <CardMedia
+        className={classes.media}
+        image={userInfo.picture}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {userInfo.title} {userInfo.firstName} {userInfo.lastName}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
-            This impressive paella is a perfect party dish and a fun meal to cook together with your
-            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+            minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+            again without stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don’t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
           </Typography>
         </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="share">
-            <ShareIcon />
-          </IconButton>
-          <IconButton
-            className={clsx(classes.expand, {
-              [classes.expandOpen]: expanded,
-            })}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-              minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-              heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-              browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-              and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-              pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-              saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-              without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-              medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-              again without stirring, until mussels have opened and rice is just tender, 5 to 7
-              minutes more. (Discard any mussels that don’t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
-          </CardContent>
-        </Collapse>
-      </Card>
+      </Collapse>
+    </Card>
+
+      // if (userInfo === null){
+      //   return null
+      // } 
+    return (
+      view
+      //   <Card className={classes.root}>
+      //   <CardHeader
+      //     avatar={
+      //       <Avatar aria-label="recipe" className={classes.avatar}>
+      //         {userInfo.firstName}
+      //       </Avatar>
+      //     }
+      //     action={
+      //       <IconButton aria-label="settings">
+      //         <MoreVertIcon />
+      //       </IconButton>
+      //     }
+      //     title= {props.firstName}
+      //     subheader={props.title}
+      //   />
+      //   <CardMedia
+      //     className={classes.media}
+      //     image={userInfo.picture}
+      //     title="Paella dish"
+      //   />
+      //   <CardContent>
+      //     <Typography variant="body2" color="textSecondary" component="p">
+      //       {userInfo.title} {userInfo.firstName} {userInfo.lastName}
+      //     </Typography>
+      //   </CardContent>
+      //   <CardActions disableSpacing>
+      //     <IconButton aria-label="add to favorites">
+      //       <FavoriteIcon />
+      //     </IconButton>
+      //     <IconButton aria-label="share">
+      //       <ShareIcon />
+      //     </IconButton>
+      //     <IconButton
+      //       className={clsx(classes.expand, {
+      //         [classes.expandOpen]: expanded,
+      //       })}
+      //       onClick={handleExpandClick}
+      //       aria-expanded={expanded}
+      //       aria-label="show more"
+      //     >
+      //       <ExpandMoreIcon />
+      //     </IconButton>
+      //   </CardActions>
+      //   <Collapse in={expanded} timeout="auto" unmountOnExit>
+      //     <CardContent>
+      //       <Typography paragraph>Method:</Typography>
+      //       <Typography paragraph>
+      //         Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+      //         minutes.
+      //       </Typography>
+      //       <Typography paragraph>
+      //         Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+      //         heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+      //         browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+      //         and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+      //         pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+      //         saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+      //       </Typography>
+      //       <Typography paragraph>
+      //         Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+      //         without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+      //         medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+      //         again without stirring, until mussels have opened and rice is just tender, 5 to 7
+      //         minutes more. (Discard any mussels that don’t open.)
+      //       </Typography>
+      //       <Typography>
+      //         Set aside off of the heat to let rest for 10 minutes, and then serve.
+      //       </Typography>
+      //     </CardContent>
+      //   </Collapse>
+      // </Card>
     );
     
 }
